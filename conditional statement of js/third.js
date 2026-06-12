84c990580578bdb9d5c61fcb8bd7fff250546210
avikamane <avika.mane.cc@gmail.com>
2026-06-12 21:28:27 +0530

    added array functions and template litrals

--- conditional statement of js/third.js
+++ conditional statement of js/third.js
@@ -111,5 +111,78 @@ for(let key in person){
     console.log(key, ":", person[key]);
 }
 
+// array 
+let numbers = [1, 2, 3, 4, 5];
+console.log(numbers[0]);
+console.log(numbers.length);
+numbers.push(6);
+console.log(numbers);
+numbers.pop();
+console.log(numbers);
+
+// object
+let student = {
+    name: "Avika",
+    age: 20,
+    city: "Pune"
+};
+console.log(student.name);
+console.log(student["age"]);
+student.age = 21;
+console.log(student);
+
+// functions
+function greet(name){
+    console.log("Hello, " + name);
+}
+greet("Avika");
+
+function add(a, b){
+    return a + b;
+}
+let sum1 = add(5, 10);
+console.log("Sum =", sum1);
+
+function isEven(num){
+    return num % 2 === 0;
+}
+console.log(isEven(4));
+console.log(isEven(5));
+
+// arrow function
+let greet2 = (name) => {
+    console.log("Hello, " + name);
+}
+greet2("Avika");
+let add2 = (a, b) => {
+    return a + b;
+}   
+let sum2 = add2(5, 10);
+console.log("Sum =", sum2);
+
+// template literals
+let name = "Avika";
+let age1 = 20;
+console.log(`My name is ${name} and I am ${age1} years old.`);  
+
+// destructuring
+let person1 = {
+    name: "Avika",
+    age: 20,
+    city: "Pune"
+};
+let {name: personName, age: personAge} = person1;
+console.log("Name:", personName);
+console.log("Age:", personAge); 
+
+// spread operator
+let arr1 = [1, 2, 3];
+let arr2 = [...arr1, 4, 5];
+console.log(arr2);
+let obj1 = {a: 1, b: 2};
+let obj2 = {...obj1, c: 3};
+console.log(obj2);
+
+
 
 
